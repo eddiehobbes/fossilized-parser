3eeef13f2faeff80a7aa4563d083188cd2fe4733
Edward Yee <osuyee.82@gmail.com>
2015-06-06 13:35:18 -0400

    feat(foss-parser):initial commit

---
+++ jurassic/Prehistoric/IO/FileDownload.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prehistoric
+{
+    public class FileDownload
+    {
+        public void Download(string address, string fileName)
+        {
+            WebClient client = new WebClient();
+
+            client.DownloadFile(address, fileName);
+        }
+    }
+}
